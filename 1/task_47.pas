516032732ec1873cd2705da9ce15830eaa4e63c4
Xar0n <toni.neczvetaev@bk.ru>
2018-09-18 22:34:36 +0800

    fix errors laba task47

--- 1/task_47.pas
+++ 1/task_47.pas
@@ -8,12 +8,14 @@ var X, Y, Z: real;
 begin
 write('Введите значения сторон треугольника X, Y, Z > 0 через пробел или через ENTER: ');
 readln(X, Y, Z);
-if (X > 0) and (Y > 0) and (Z > 0) and (X+Y > Z) and (X+Z > Y) and (Y+Z > X) Then
+if (X > 0) and (Y > 0) and (Z > 0) and (X+Y > Z) and (X+Z > Y) and (Y+Z > X) then
 begin
     write('Треугольник построить можно');
-    if ((X*X + Y*Y) > Z*Z) and ((Y*Y + Z*Z) > X*X) and ((X*X + Z*Z) > Y*Y) Then
+    if ((X*X + Y*Y) > Z*Z) and ((Y*Y + Z*Z) > X*X) and ((X*X + Z*Z) > Y*Y) then
     begin
          write(' и он остроугольный');
     end
+    else write(', но он не остроугольный');
 end
+else write('Тругольник построить нельзя');
 end.
