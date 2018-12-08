15961480d07698c68d2f26100df841a72030f6ba
Xar0n <toni.neczvetaev@bk.ru>
2018-12-08 21:42:56 +0800

    lab 4 fix errors

--- 4/task_4_1.pas
+++ 4/task_4_1.pas
@@ -1,32 +1,34 @@
 {
 В заданный непустой текст входят только цифры и буквы.
-Длина текста не более 12 символов.Определить, удовлетворяется ли следующее свойство:
+Длина текста не более 12 символов.
+Определить, удовлетворяется ли следующее свойство:
 Текст является восьмеричной записью числа, кратного 4;
 }
 program task_4_1;
 uses crt;
 var
-   text: string;
+   sign, last_sign:char;
    symbols: set of char;
-   i, j, s: integer;
+   i: integer;
    flag: boolean;
 begin
-cls;
-symbols := ['1' .. '8'];
-s := 0;
+i := 12;
+flag := false;
+symbols := ['0' .. '7'];
+write('Введите строку:');
 repeat
-   write('Введите текст <= 12 символам:');
-   readln(text);
-until length(text) <= 12;
-for i := 1 to length(text) do
-begin
-   if (text[i] in symbols) = true then s := s + (ord(text[i])-48)
-   else
+   sign := readkey;
+   if sign = #13 then
    begin
-       write('Найден символ не принадлежащий заданному множеству 8-ичных чисел');
-       exit;
-   end
-end;
-if s mod 4 = 0 then write('Текст является восьмеричной записью числа, кратного 4')
+       flag := true;
+       break;
+   end;
+   write(sign);
+   last_sign := sign;
+   dec(i);
+until (i = 0) or (sign in symbols = false);
+writeln;
+if flag and ((last_sign = '4') or (last_sign = '0')) then write('Текст является восьмеричной записью числа, кратного 4')
+else if (sign = '4') or (sign = '0') then write('Текст является восьмеричной записью числа, кратного 4')
 else write('Текст не является восьмеричной записью числа, кратного 4');
 end.
